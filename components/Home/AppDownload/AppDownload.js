2282f0f15d40c365e83aebfc178cf46d9c97e34f
briaguya <briaguya>
2021-10-21 07:14:18 -0400

    run prettier on everything

--- components/Home/AppDownload/AppDownload.js
+++ components/Home/AppDownload/AppDownload.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React from 'react';
 
 const AppDownload = () => {
-    return(
-        <section className="section cta-section theme">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
-                        <h2>Download and Use our App and Software</h2>
-                    </div>
-                    <div className="col-md-5 text-center text-md-end">
-                        <a href="https://play.google.com/store/apps/details?id=org.metabrainz.android" target="_blank" rel="noopener noreferrer" className="btn d-inline-flex align-items-center">
-                            <i className="fab fa-google-play"/><span>Google play</span>
-                        </a>
-                        <a href="https://f-droid.org/en/packages/org.metabrainz.android/" target="_blank" rel="noopener noreferrer" className="btn d-inline-flex align-items-center">
-                            <i className="fab fa-android"/><span>F-Droid</span>
-                        </a>
-                        <a href="https://picard.musicbrainz.org" target="_blank" rel="noopener noreferrer" className="btn d-inline-flex align-items-center">
-                            <i className="fa fa-laptop"/><span>Picard</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="section cta-section theme">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
+            <h2>Download and Use our App and Software</h2>
+          </div>
+          <div className="col-md-5 text-center text-md-end">
+            <a
+              href="https://play.google.com/store/apps/details?id=org.metabrainz.android"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn d-inline-flex align-items-center"
+            >
+              <i className="fab fa-google-play" />
+              <span>Google play</span>
+            </a>
+            <a
+              href="https://f-droid.org/en/packages/org.metabrainz.android/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn d-inline-flex align-items-center"
+            >
+              <i className="fab fa-android" />
+              <span>F-Droid</span>
+            </a>
+            <a
+              href="https://picard.musicbrainz.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn d-inline-flex align-items-center"
+            >
+              <i className="fa fa-laptop" />
+              <span>Picard</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default AppDownload;
--- components/Home/Facts/Facts.js
+++ components/Home/Facts/Facts.js
@@ -1,56 +1,52 @@
-import React from "react";
+import React from 'react';
 
 const Facts = () => {
-    return(
-        <section id="counts" className="counts theme">
-            <div className="container" data-aos="fade-up">
-
-                <div className="row gy-4">
-
-                    <div className="col-lg-3 col-md-6">
-                        <div className="count-box">
-                            <i className="bi bi-music-note-list"/>
-                            <div>
-                                <span>1.88 M</span>
-                                <p>Artists</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-3 col-md-6">
-                        <div className="count-box">
-                            <i className="bi bi-journal-richtext"/>
-                            <div>
-                                <span>3.00 M</span>
-                                <p>Releases</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-3 col-md-6">
-                        <div className="count-box">
-                            <i className="bi bi-headset"/>
-                            <div>
-                                <span>35.20 M</span>
-                                <p>Tracks</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-3 col-md-6">
-                        <div className="count-box">
-                            <i className="bi bi-people"/>
-                            <div>
-                                <span>2.18 M</span>
-                                <p>Editors</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+  return (
+    <section id="counts" className="counts theme">
+      <div className="container" data-aos="fade-up">
+        <div className="row gy-4">
+          <div className="col-lg-3 col-md-6">
+            <div className="count-box">
+              <i className="bi bi-music-note-list" />
+              <div>
+                <span>1.88 M</span>
+                <p>Artists</p>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="count-box">
+              <i className="bi bi-journal-richtext" />
+              <div>
+                <span>3.00 M</span>
+                <p>Releases</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="count-box">
+              <i className="bi bi-headset" />
+              <div>
+                <span>35.20 M</span>
+                <p>Tracks</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="count-box">
+              <i className="bi bi-people" />
+              <div>
+                <span>2.18 M</span>
+                <p>Editors</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Facts;
